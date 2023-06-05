6914ec968f173c12a289d8242bd0052d21adf2d6
saurabhpanchal <61181093+saurabhpanchal8950@users.noreply.github.com>
2023-06-05 10:10:07 +0530

    Add files via upload

---
+++ src/helper/helper.js
@@ -0,0 +1,43 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import axios from 'axios'
+
+export function attempts_Number(result){
+    return result.filter(r => r !== undefined).length;
+}
+
+export function earnPoints_Number(result, answers, point){
+    return result.map((element, i) => answers[i] === element).filter(i => i).map(i => point).reduce((prev, curr) => prev + curr, 0);
+}
+
+export function flagResult(totalPoints, earnPoints){
+    return (totalPoints * 50 / 100) < earnPoints; /** earn 50% marks */
+}
+
+/** check user auth  */
+export function CheckUserExist({ children }){
+    const auth = useSelector(state => state.result.userId)
+    return auth ? children : <Navigate to={'/'} replace={true}></Navigate>
+}
+
+/** get server data */
+export async function getServerData(url, callback){
+    const data = await (await axios.get(url))?.data;
+    return callback ? callback(data) : data;
+}
+export function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[i]];
+    }
+    return shuffledArray;
+}
+
+
+
+/** post server data */
+export async function postServerData(url, result, callback){
+    const data = await (await axios.post(url, result))?.data;
+    return callback ? callback(data) : data;
+}
